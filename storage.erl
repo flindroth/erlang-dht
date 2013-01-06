235a61b648534d4ab6cb64c5a6369d1b7f1bca6a
Fredrik Lindroth <fredriklindroth@gmail.com>
2013-01-07 00:44:18 +0100

    Added file reading.

--- storage.erl
+++ storage.erl
@@ -1,2 +1,27 @@
 -module(storage).
+-export([store/2,retrieve/1,init/1]).
+
+store(Key,Value) ->
+	storage ! {store,Key,Value}.
+
+retrieve(Key) ->
+	storage ! {get,self(),Key},
+	receive
+		{result,Value} -> Value;
+		_ -> error
+	end.
+
+
+storage_srv(Path) ->
+	receive
+		{get, PID, Key} ->
+			PID ! file:read_file(Path+Key),
+			storage_srv(Path);
+		Other ->
+			io:format("Unknown message: ~p~n", [Other]),
+			storage_srv(Path)
+		end.
+
+init(Path) ->
+	register(storage,spawn(storage,storage_srv,[Path])).
 
