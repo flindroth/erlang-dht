c08e4d4a0b0860dedc2d0990767e842965f66a01
Fredrik Lindroth <fredriklindroth@gmail.com>
2013-01-13 13:02:07 +0100

    Added leaf table mechanism

---
+++ leaf_table.erl
@@ -0,0 +1,21 @@
+-module(leaf_table).
+-export([list_insert/2]).
+
+list_insert(Leaf,LeafTable) ->
+	list_insert([],Leaf,LeafTable).
+
+list_insert(Acc,{ID,Node},[]) ->
+	lists:reverse([{ID,Node} | Acc]);
+list_insert(Acc,{ID,Node},[{HID,HNode} | T]) ->
+	if
+		ID > HID ->
+			list_insert([{HID,HNode} | Acc],{ID,Node},T);
+		ID == HID ->
+			lists:reverse(Acc,[{ID,Node} | T]);
+		true ->
+			lists:reverse(Acc,[{ID,Node} | [{HID,HNode} | T]])	
+	end.	
+	
+
+add(LeafTable, {ID,Node}) ->
+	list_insert({ID,Node},LeafTable).
